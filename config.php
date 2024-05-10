6c259e037dfb8797d2f341984fa6dfd3894481a7
Thiago Morellato <thiagomorellatoa@gmail.com>
2024-05-10 11:12:56 -0300

    abrigos

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost:3306";
+$username = "abrigopet";
+$password = "o~m6r57P4";
+$dbname = "sistema_abrigo_animais";
+?>
--- get_abrigos_animais.php
+++ get_abrigos_animais.php
@@ -1,9 +1,6 @@
 <?php
 
-$servername = "localhost:3306";
-$username = "abrigopet";
-$password = "o~m6r57P4";
-$dbname = "sistema_abrigo_animais";
+require_once 'config.php';
 
 // Conecta ao banco de dados
 $conn = mysqli_connect($servername, $username, $password, $dbname);

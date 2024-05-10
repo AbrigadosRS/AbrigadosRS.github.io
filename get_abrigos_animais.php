06897ddc7acfba5883148ff8099d0a18030d5916
Thiago Morellato <thiagomorellatoa@gmail.com>
2024-05-10 14:08:20 -0300

    abrigos

--- get_abrigos_animais.php
+++ get_abrigos_animais.php
@@ -32,7 +32,7 @@ if ($result->num_rows > 0) {
         }
 
         // Output dos dados em linhas estilo botão
-        echo "<table class='abrigos-table'>";
+        echo "<table class='abrigos-table' style='border-radius: 10px;'>";
         echo "<tr>";
         echo "<td>" . $row["nome"] . "</td>";
         echo "<td>" . $row["endereco"] . "</td>";
